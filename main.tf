a9a8b96fb76da66ef43f7e7a40c369852419cac6
ziggy67jh <54290833+ziggy67jh@users.noreply.github.com>
2024-02-19 18:27:05 -0500

    fixed "

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module "blog_sg" {
   name    = "blog_new"
 
   vpc_id = data.aws_vpc.default.id
-  ingress_rules       = ["http-80-tcp,"https-443-tcp"]
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules       = ["all-all"]
